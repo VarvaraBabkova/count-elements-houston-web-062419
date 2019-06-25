3546707da2d023f0fb3371e9b2ec492b9552289f
Varvara Babkova <varvara.munday@gmail.com>
2019-06-25 13:27:45 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,6 +1,9 @@
 def count_elements(array)
   hash = {}
   ar_uniq = array.uniq
+  ar_uniq.each do |key|
+    hash[key] = 0
+  end
   array.each do |elem|
     ar_uniq.each do |key|
         if key == elem
